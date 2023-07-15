17fa9b2d852049792d9d0736d4a0e99295b8257c
navanath markad <navanath96@gmail.com>
2023-07-15 22:13:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Formvalidation from "./components/Formvalidation"
+
+let App= ()=>{
+return (
+    <div>
+        <Formvalidation />
+    </div>
+)
+}
+
+export default App
